e1eb8f787bc4ea2f990da2e66e11c4686d93bf12
ulusalemin56 <mulusal456@gmail.com>
2023-09-13 21:38:24 +0300

    Hackerrank - 'Solve me First' algorithm question solved.

---
+++ hackerrank/1-solveMeFirs.kt
@@ -0,0 +1,12 @@
+package hackerrank
+
+fun solveMeFirst(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun main() {
+    val num1 = 2
+    val num2 = 3
+    val sum = solveMeFirst(num1, num2)
+    println(sum)
+}
